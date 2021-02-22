17021fd4c960bef4f8a8e8bd5fd17f046806727e
Rashed Khan <abir97bd@gmail.com>
2021-02-22 22:13:40 +0600

    second commit

---
+++ gpa cal.py
@@ -0,0 +1,31 @@
+a = input("Enter your GPA : ");
+if(4.00 >= float(a)):{
+    print("A+")
+}
+elif(3.75 >= float(a)):{
+    print("A")
+}
+elif(3.50 >= float(a)):{
+    print("A-")
+}
+elif(3.25 >= float(a)):{
+    print("B+")
+}
+elif(3.00 >= float(a)):{
+    print("B")
+}
+elif(2.75 >= float(a)):{
+    print("B-")
+}
+elif(2.50 >= float(a)):{
+    print("C+")
+}
+elif(2.25 >= float(a)):{
+    print("C")
+}
+elif(2.00 >= float(a)):{
+    print("C-")
+}
+else:{
+    print("F")
+}
---
+++ str slice.py
@@ -0,0 +1,3 @@
+mystr = "abu rashed khan";
+print(len(mystr))
+print(mystr[4:10:2 ])
